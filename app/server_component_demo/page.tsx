a467a6363eef7203b904188ada26d825f75c5d95
曲木车格 <qumuchege@qumuchegedeMacBook-Pro.local>
2022-11-14 13:55:09 +0800

    fix vercel build error

--- app/server_component_demo/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function Page() {
-  const result: string = await new Promise((res) => {
-    setTimeout(() => res("yes, it is a server component"), 2000);
-  });
-  return <div>{result}</div>;
-}
